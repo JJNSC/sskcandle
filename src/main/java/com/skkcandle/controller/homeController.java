f39bdbbc682d5076f345f837fae9f301a1db0157
Üc207 Pr4 4f57t9 <myway5231@naver.com>
2023-08-09 14:13:51 +0900

    체크아웃 푸쉬

--- src/main/java/com/skkcandle/controller/homeController.java
+++ src/main/java/com/skkcandle/controller/homeController.java
@@ -3,10 +3,8 @@ package com.skkcandle.controller;
 import javax.annotation.Resource;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.skkcandle.dto.Review;
 import com.skkcandle.service.ReviewService;
 
 @Controller
