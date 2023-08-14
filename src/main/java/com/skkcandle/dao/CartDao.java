718ea915c847f0ee2dc63a12111d7cc68b521dd3
sjcho21 <sjcho21@gamil.com>
2023-08-14 14:07:22 +0900

    cart dao 수정

--- src/main/java/com/skkcandle/dao/CartDao.java
+++ src/main/java/com/skkcandle/dao/CartDao.java
@@ -10,18 +10,13 @@ import com.skkcandle.dto.Cart;
 @Mapper
 public interface CartDao {
 	
-	/*public List<Cart> cartMoney();*/    
 	public void insert(Cart cart); //장바구니 추가
     public List<Cart> listCart(int userId); //장바구니 목록
     public void delete(Cart cart); //장바구니 개별 삭제
     public void deleteAll(String userId); //장바구니 비우기
-  //  public void update(Cart cart); 
-   // public int sumMoney(String userId); //장바구니 금액 합계
-  //  public int countCart(String userId, int productId); //장바구니 상품 갯수
     public void updateCart(Cart cart); //장바구니 수정 
     public void modifyCart(Cart cart);
     public int countCart(Cart cart); //db에 같은 상품이 있는지 가져오기 있으면 1 없으면 0 리턴
     public int selectCount(Cart cart);
     public ArrayList<Integer> getListCart(int userId);
-  
 }
